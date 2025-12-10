34d665d7a55a1068eee106b9537465f7cddef8dd
ysdesha <ysdesha@gmail.com>
2025-12-10 20:06:17 +0530

    Add files via upload

---
+++ erc_gazebo_sensors/launch/check_urdf.launch.py
@@ -0,0 +1,45 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    
+    pkg_erc_gazebo_sensors = FindPackageShare('erc_gazebo_sensors')
+    default_rviz_config_path = PathJoinSubstitution([pkg_erc_gazebo_sensors, 'rviz', 'urdf.rviz'])
+
+    # Show joint state publisher GUI for joints
+    gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
+                                    description='Flag to enable joint_state_publisher_gui')
+    
+    # RViz config file path
+    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
+                                    description='Absolute path to rviz config file')
+    
+
+    # URDF model path within the erc_gazebo_sensors package
+    model_arg = DeclareLaunchArgument(
+        'model', default_value='erc_bot.urdf',
+        description='Name of the URDF description to load'
+    )
+
+    # Use built-in ROS2 URDF launch package with our own arguments
+    urdf = IncludeLaunchDescription(
+        PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
+        launch_arguments={
+            'urdf_package': 'erc_gazebo_sensors',
+            'urdf_package_path': PathJoinSubstitution(['urdf', LaunchConfiguration('model')]),
+            'rviz_config': LaunchConfiguration('rvizconfig'),
+            'jsp_gui': LaunchConfiguration('gui')}.items()
+    )
+
+    launchDescriptionObject = LaunchDescription()
+
+    launchDescriptionObject.add_action(gui_arg)
+    launchDescriptionObject.add_action(rviz_arg)
+    launchDescriptionObject.add_action(model_arg)
+    launchDescriptionObject.add_action(urdf)
+
+    return launchDescriptionObject
---
+++ erc_gazebo_sensors/launch/spawn_robot.launch.py
@@ -0,0 +1,154 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    pkg_erc_gazebo_sensors = get_package_share_directory('erc_gazebo_sensors')
+
+    gazebo_models_path, ignore_last_dir = os.path.split(pkg_erc_gazebo_sensors)
+    os.environ["GZ_SIM_RESOURCE_PATH"] += os.pathsep + gazebo_models_path
+
+    rviz_launch_arg = DeclareLaunchArgument(
+        'rviz', default_value='true',
+        description='Open RViz'
+    )
+
+    rviz_config_arg = DeclareLaunchArgument(
+        'rviz_config', default_value='rviz.rviz',
+        description='RViz config file'
+    )
+
+    world_arg = DeclareLaunchArgument(
+        'world', default_value='home.sdf',
+        description='Name of the Gazebo world file to load'
+    )
+
+    model_arg = DeclareLaunchArgument(
+        'model', default_value='erc_bot.urdf',
+        description='Name of the URDF description to load'
+    )
+
+    x_arg = DeclareLaunchArgument(
+        'x', default_value='2.5',
+        description='x coordinate of spawned robot'
+    )
+
+    y_arg = DeclareLaunchArgument(
+        'y', default_value='1.5',
+        description='y coordinate of spawned robot'
+    )
+
+    yaw_arg = DeclareLaunchArgument(
+        'yaw', default_value='-1.5707',
+        description='yaw angle of spawned robot'
+    )
+
+    sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time', default_value='True',
+        description='Flag to enable use_sim_time'
+    )
+
+    # Define the path to your URDF or Xacro file
+    urdf_file_path = PathJoinSubstitution([
+        pkg_erc_gazebo_sensors,  # Replace with your package name
+        "urdf",
+        LaunchConfiguration('model')  # Replace with your URDF or Xacro file
+    ])
+
+    world_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_erc_gazebo_sensors, 'launch', 'world.launch.py'),
+        ),
+        launch_arguments={
+        'world': LaunchConfiguration('world'),
+        }.items()
+    )
+
+    # Launch rviz
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', PathJoinSubstitution([pkg_erc_gazebo_sensors, 'rviz', LaunchConfiguration('rviz_config')])],
+        condition=IfCondition(LaunchConfiguration('rviz')),
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ]
+    )
+
+    # Spawn the URDF model using the `/world/<world_name>/create` service
+    spawn_urdf_node = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-name", "erc_bot",
+            "-topic", "robot_description",
+            "-x", LaunchConfiguration('x'), "-y", LaunchConfiguration('y'), "-z", "0.5", "-Y", LaunchConfiguration('yaw')  # Initial spawn position
+        ],
+        output="screen",
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ]
+    )
+
+    # Node to bridge /cmd_vel and /odom
+    gz_bridge_node = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist",
+            "/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry",
+            "/joint_states@sensor_msgs/msg/JointState@gz.msgs.Model",
+            "/tf@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V",
+        ],
+        output="screen",
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ]
+    )
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[
+            {'robot_description': Command(['xacro', ' ', urdf_file_path]),
+             'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ],
+        remappings=[
+            ('/tf', 'tf'),
+            ('/tf_static', 'tf_static')
+        ]
+    )
+
+    trajectory_node = Node(
+        package='mogi_trajectory_server',
+        executable='mogi_trajectory_server',
+        name='mogi_trajectory_server'
+    )
+
+    launchDescriptionObject = LaunchDescription()
+
+    launchDescriptionObject.add_action(rviz_launch_arg)
+    launchDescriptionObject.add_action(rviz_config_arg)
+    launchDescriptionObject.add_action(world_arg)
+    launchDescriptionObject.add_action(model_arg)
+    launchDescriptionObject.add_action(x_arg)
+    launchDescriptionObject.add_action(y_arg)
+    launchDescriptionObject.add_action(yaw_arg)
+    launchDescriptionObject.add_action(sim_time_arg)
+    launchDescriptionObject.add_action(world_launch)
+    launchDescriptionObject.add_action(rviz_node)
+    launchDescriptionObject.add_action(spawn_urdf_node)
+    launchDescriptionObject.add_action(gz_bridge_node)
+    launchDescriptionObject.add_action(robot_state_publisher_node)
+    launchDescriptionObject.add_action(trajectory_node)
+
+    return launchDescriptionObject
